808ec09971e415cb1a38b0d91fb238cfcfcd9252
Addagiri lakshmi priyanka <141920060+addagirilakshmipriyanka@users.noreply.github.com>
2026-04-01 23:23:15 +0530

    Add files via upload

---
+++ C#.net/Assesments/C#_Basic_Assignment_2/Programme-8/Program.cs
@@ -0,0 +1,24 @@
+﻿//8.Write a program to print the series: 0,1,4,9,16,.....625
+
+namespace Programme_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int last = 625;
+            int i = 1;
+            while (total < last)
+            {
+                if (i % 2 == 1)
+                {
+                    total = total + i;
+                    Console.WriteLine(total);
+                }
+                i++;
+
+            }
+        }
+    }
+}
