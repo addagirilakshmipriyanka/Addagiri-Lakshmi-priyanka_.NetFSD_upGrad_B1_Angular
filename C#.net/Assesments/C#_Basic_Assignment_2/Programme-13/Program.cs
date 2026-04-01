afb7cbec1d72978bafc611538f5198f8d6ee171c
Addagiri lakshmi priyanka <141920060+addagirilakshmipriyanka@users.noreply.github.com>
2026-04-01 23:29:17 +0530

    Add files via upload

---
+++ C#.net/Assesments/C#_Basic_Assignment_2/Programme-13/Program.cs
@@ -0,0 +1,28 @@
+﻿//13.	Write a program in C# to find the largest of the given three numbers. Accept the numbers from the users.
+namespace Programme_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number 1=");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter number 2=");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter number 3=");
+            int num3 = int.Parse(Console.ReadLine());
+            if (num1 >= num2 && num1 >= num3)
+            {
+                Console.WriteLine(num1 + " is largest");
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                Console.WriteLine(num2 + " is largest");
+            }
+            else
+            {
+                Console.WriteLine(num3 + " is largest");
+            }
+        }
+    }
+        }
---
+++ C#.net/Assesments/C#_Basic_Assignment_2/programme-14/Program.cs
@@ -0,0 +1,36 @@
+﻿//14 Write a program in C# to find the smallest of five numbers accepted from the user.
+
+namespace programme_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the  5 numbers");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+            int num5 = int.Parse(Console.ReadLine());
+            if(num1<num2 && num1<num3 && num1<num4 && num1 < num5)
+            {
+                Console.WriteLine("smallest number is " + num1);
+            }
+            else if (num2 < num1 && num2 < num3 && num2 < num4 && num2 < num5)
+            {
+                Console.WriteLine("smallest number is " + num2);
+            }
+            else if (num3 < num1 && num3 < num2 && num3 < num4 && num3 < num5)
+            {
+                Console.WriteLine("smallest number is " + num3);
+            }
+            else if (num4 < num1 && num4 < num2 && num4 < num3 && num4 < num5)
+            {
+                Console.WriteLine("smallest number is " + num4);
+            }
+            else
+            {
+                Console.WriteLine("smallest number is"+num5);            }
+        }
+    }
+}
