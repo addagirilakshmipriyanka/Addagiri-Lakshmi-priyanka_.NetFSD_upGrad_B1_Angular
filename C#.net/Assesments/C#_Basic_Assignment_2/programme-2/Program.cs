4e2b832d0d5bed6b3ee52c2970a712ec286ea471
Addagiri lakshmi priyanka <141920060+addagirilakshmipriyanka@users.noreply.github.com>
2026-04-01 23:12:18 +0530

    Add files via upload

---
+++ C#.net/Assesments/C#_Basic_Assignment_2/programme-2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*2.	Write a program in C# to accept the name of the user as a command line argument and greet the user as:
+	“Hi! username 
+	Welcome to the world of C#”*/
+namespace programme_2
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string username = args[0];
+                Console.WriteLine($"Hi! {username}");
+                Console.WriteLine("Welcome to the world of C#");
+
+            }
+            else
+            {
+                Console.WriteLine("Please provide your name as a command-line argument.");
+            }
+        }
+    }
+}
