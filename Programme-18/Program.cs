4677cc8bd2f0ce4910dd81c92d6108d425d18444
Addagiri lakshmi priyanka <141920060+addagirilakshmipriyanka@users.noreply.github.com>
2026-04-01 23:34:13 +0530

    Add files via upload

---
+++ Programme-18/Program.cs
@@ -0,0 +1,24 @@
+﻿//18.	Write a program in C# to accept two words from user and find out if they are same.
+namespace Programme_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first word: ");
+            string word1 = Console.ReadLine();
+
+            Console.Write("Enter second word: ");
+            string word2 = Console.ReadLine();
+
+            if (word1 == word2)
+            {
+                Console.WriteLine("Both words are same");
+            }
+            else
+            {
+                Console.WriteLine("Words are different");
+            }
+        }
+    }
+}
---
+++ Programme-19/Program.cs
@@ -0,0 +1,25 @@
+﻿//19 Write a program in C# to accept a word and to find out whether the given word is a palindrome or not.
+namespace Programme_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+            string reverse = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reverse += word[i];
+            }
+            if (word == reverse)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not a palindrome");
+            }
+        }
+    }
+}
