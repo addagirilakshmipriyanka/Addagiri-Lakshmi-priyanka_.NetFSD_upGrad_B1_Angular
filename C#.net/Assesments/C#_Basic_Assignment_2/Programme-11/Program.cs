bd11818f3b88fe71305630e983c6b870d248c8a3
Addagiri lakshmi priyanka <141920060+addagirilakshmipriyanka@users.noreply.github.com>
2026-04-01 23:31:08 +0530

    Add files via upload

---
+++ C#.net/Assesments/C#_Basic_Assignment_2/Programme-11/Program.cs
@@ -0,0 +1,25 @@
+﻿//11.	Write a program in C# to find the multiplication table of the given number till 20.
+namespace Programme_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int num = int.Parse(Console.ReadLine());
+            if (num > 20)
+            {
+                Console.WriteLine("enter  a number less than 20");
+            }
+            else
+            {   
+                int i = 1;
+                while (i <= 10)
+                {
+                    Console.WriteLine(num  + "X" + i + "="+num * i);
+                    i++;
+                }
+            }
+        }
+    }
+}
