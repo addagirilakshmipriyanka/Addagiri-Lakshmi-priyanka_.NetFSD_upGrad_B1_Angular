0ed6e4e45af88021effd89d61b830543bfb017af
Addagiri lakshmi priyanka <141920060+addagirilakshmipriyanka@users.noreply.github.com>
2026-04-01 23:10:31 +0530

    Add files via upload

---
+++ C#.net/Assesments/C#_Basic_Assignment_2/proramme-1/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1.	Write a program in C# to display a message as follows: “Welcome to the world of C#”
+namespace proramme_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the world of C#");
+        }
+    }
+}
