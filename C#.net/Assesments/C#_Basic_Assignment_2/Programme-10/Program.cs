6a2b7e0cb91c3221d1aa61e486dee93b5a33cd38
Addagiri lakshmi priyanka <141920060+addagirilakshmipriyanka@users.noreply.github.com>
2026-04-01 23:24:34 +0530

    Add files via upload

---
+++ C#.net/Assesments/C#_Basic_Assignment_2/Programme-10/Program.cs
@@ -0,0 +1,23 @@
+﻿// 10.	Write a program in C# to generate a Fibonacci series till 40.
+namespace Programme_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 =1;
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            while (num3 < 40)
+            {
+                Console.WriteLine(num3);
+               
+                num1 = num2;
+                num2 = num3;
+                num3 = num1 + num2;
+            }
+        }
+    }
+}
---
+++ C#.net/Assesments/C#_Basic_Assignment_2/Programme-9/Program.cs
@@ -0,0 +1,22 @@
+﻿//9.Write a program in C# to find the factorial of the given number.
+
+namespace Programme_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 1;
+            Console.WriteLine("Enter the  number");
+            int num = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= num)
+            {
+                fact = fact * i;
+                i++;
+            }
+            Console.WriteLine($"fcatorial of the given number={fact}");
+           
+        }
+    }
+}
